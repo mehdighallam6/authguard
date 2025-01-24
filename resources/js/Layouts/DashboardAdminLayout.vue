620e084c22f659e675128e32a9b6e28c01d47238
mehdighallam6 <mehdighallam6@gmail.com>
2025-01-24 23:24:28 +0100

    admin dashboard with its components

---
+++ resources/js/Layouts/DashboardAdminLayout.vue
@@ -0,0 +1,178 @@
+<script setup lang="ts">
+import { Button } from '@/Components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/Components/ui/dropdown-menu';
+import { Sheet, SheetContent, SheetTrigger } from '@/Components/ui/sheet';
+import {
+    CircleUser,
+    Home,
+    LineChart,
+    Menu,
+    Package,
+    Package2,
+    ShoppingCart,
+    Users,
+} from 'lucide-vue-next';
+</script>
+
+<template>
+    <div
+        class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
+    >
+        <div class="hidden border-r bg-muted/40 md:block">
+            <div class="flex h-full max-h-screen flex-col gap-2">
+                <div
+                    class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6"
+                >
+                    <a
+                        href="/"
+                        class="flex items-center gap-2 font-semibold"
+                    >
+                        <Package2 class="h-6 w-6" />
+                        <span class="">AuthGuard</span>
+                    </a>
+                </div>
+                <div class="flex-1">
+                    <nav
+                        class="grid items-start px-2 text-sm font-medium lg:px-4"
+                    >
+                        <a
+                            href="/"
+                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                        >
+                            <Home class="h-4 w-4" />
+                            Dashboard
+                        </a>
+                        <a
+                            href="#"
+                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                        >
+                            <ShoppingCart class="h-4 w-4" />
+                            Orders
+                        </a>
+                        <a
+                            href="#"
+                            class="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                        >
+                            <Package class="h-4 w-4" />
+                            Products
+                        </a>
+                        <a
+                            href="#"
+                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                        >
+                            <Users class="h-4 w-4" />
+                            Customers
+                        </a>
+                        <a
+                            href="#"
+                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                        >
+                            <LineChart class="h-4 w-4" />
+                            Analytics
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <header
+                class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6"
+            >
+                <Sheet>
+                    <SheetTrigger as-child>
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            class="shrink-0 md:hidden"
+                        >
+                            <Menu class="h-5 w-5" />
+                            <span class="sr-only">Toggle navigation menu</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent
+                        side="left"
+                        class="flex flex-col"
+                    >
+                        <nav class="grid gap-2 text-lg font-medium">
+                            <a
+                                href="#"
+                                class="flex items-center gap-2 text-lg font-semibold"
+                            >
+                                <Package2 class="h-6 w-6" />
+                                <span class="sr-only">AuthGuard</span>
+                            </a>
+                            <a
+                                href="#"
+                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                            >
+                                <Home class="h-5 w-5" />
+                                Dashboard
+                            </a>
+                            <a
+                                href="#"
+                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                            >
+                                <ShoppingCart class="h-5 w-5" />
+                                Orders
+                            </a>
+                            <a
+                                href="#"
+                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                            >
+                                <Package class="h-5 w-5" />
+                                Products
+                            </a>
+                            <a
+                                href="#"
+                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                            >
+                                <Users class="h-5 w-5" />
+                                Customers
+                            </a>
+                            <a
+                                href="#"
+                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                            >
+                                <LineChart class="h-5 w-5" />
+                                Analytics
+                            </a>
+                        </nav>
+                    </SheetContent>
+                </Sheet>
+                <div class="w-full flex-1">
+                    <!--for search input-->
+                </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger as-child>
+                        <Button
+                            variant="secondary"
+                            size="icon"
+                            class="rounded-full"
+                        >
+                            <CircleUser class="h-5 w-5" />
+                            <span class="sr-only">Toggle user menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Settings</DropdownMenuItem>
+                        <DropdownMenuItem>Support</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </header>
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
