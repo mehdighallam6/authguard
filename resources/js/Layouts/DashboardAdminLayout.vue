4200b69cc81b7d0f827b45719c0203522171a4b4
mehdighallam6 <mehdighallam6@gmail.com>
2025-01-25 21:29:55 +0100

    Navigation in DashboardAdminLayout

--- resources/js/Layouts/DashboardAdminLayout.vue
+++ resources/js/Layouts/DashboardAdminLayout.vue
@@ -4,7 +4,6 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '@/Components/ui/dropdown-menu';
@@ -12,14 +11,40 @@ import { Sheet, SheetContent, SheetTrigger } from '@/Components/ui/sheet';
 import {
     CircleUser,
     Home,
-    LineChart,
     Menu,
-    Package,
     Package2,
-    ShoppingCart,
     Users,
+    Tag,
+    Key,
 } from 'lucide-vue-next';
 import { Link } from '@inertiajs/vue3';
+
+const navigation = [
+    {
+        name: 'Dashboard',
+        href: route('dashboard'),
+        icon: Home,
+        current: '/dashboard',
+    },
+    {
+        name: 'Users',
+        href: '',
+        icon: Users,
+        current: '/users',
+    },
+    {
+        name: 'Tags',
+        href: '',
+        icon: Tag,
+        current: '/phones',
+    },
+    {
+        name: 'Authenticators',
+        href: '',
+        icon: Key,
+        current: '/authenticators',
+    },
+];
 </script>
 
 <template>
@@ -43,41 +68,17 @@ import { Link } from '@inertiajs/vue3';
                     <nav
                         class="grid items-start px-2 text-sm font-medium lg:px-4"
                     >
-                        <a
-                            href="/"
+                        <Link
+                            v-for="item in navigation"
+                            :href="item.href"
                             class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                         >
-                            <Home class="h-4 w-4" />
-                            Dashboard
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <ShoppingCart class="h-4 w-4" />
-                            Orders
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                        >
-                            <Package class="h-4 w-4" />
-                            Products
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <Users class="h-4 w-4" />
-                            Customers
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <LineChart class="h-4 w-4" />
-                            Analytics
-                        </a>
+                            <component
+                                :is="item.icon"
+                                class="h-4 w-4"
+                            />
+                            {{ item.name }}
+                        </Link>
                     </nav>
                 </div>
             </div>
@@ -109,41 +110,17 @@ import { Link } from '@inertiajs/vue3';
                                 <Package2 class="h-6 w-6" />
                                 <span class="sr-only">AuthGuard</span>
                             </a>
-                            <a
-                                href="#"
+                            <Link
+                                v-for="item in navigation"
+                                :href="item.href"
                                 class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                             >
-                                <Home class="h-5 w-5" />
-                                Dashboard
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                            >
-                                <ShoppingCart class="h-5 w-5" />
-                                Orders
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Package class="h-5 w-5" />
-                                Products
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Users class="h-5 w-5" />
-                                Customers
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <LineChart class="h-5 w-5" />
-                                Analytics
-                            </a>
+                                <component
+                                    :is="item.icon"
+                                    class="h-5 w-5"
+                                />
+                                {{ item.name }}
+                            </Link>
                         </nav>
                     </SheetContent>
                 </Sheet>
