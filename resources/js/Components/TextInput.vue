08a2359059092ae4f2dff45b3f39ec46e8ea1442
mehdighallam6 <mehdighallam6@gmail.com>
2025-01-30 19:31:12 +0100

    Fix text input color in dark mode

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -16,7 +16,7 @@ defineExpose({ focus: () => input.value?.focus() });
 
 <template>
     <input
-        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:text-primary-foreground"
         v-model="model"
         ref="input"
     />
