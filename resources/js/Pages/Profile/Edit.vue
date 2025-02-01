1fe5ff11e75d46e1dd077fc76d8c482c1581b88e
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-01 18:47:10 +0100

    Update Edit.vue

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -45,7 +45,6 @@ const role = computed(() => page.props.auth.user.role);
     </DashboardAdminLayout>
 
     <DashboardStandardLayout v-else>
-        {{ role }}
         <div class="py-12">
             <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                 <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
