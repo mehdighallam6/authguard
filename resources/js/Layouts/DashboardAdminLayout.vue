360554817fc4ed9100bd8979cfe691697052579a
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-09 15:22:17 +0100

    Change dark mode toggle to ghost

--- resources/js/Layouts/DashboardAdminLayout.vue
+++ resources/js/Layouts/DashboardAdminLayout.vue
@@ -71,7 +71,7 @@ const mode = useColorMode();
                     </a>
                     <DropdownMenu>
                         <DropdownMenuTrigger as-child>
-                            <Button variant="outline">
+                            <Button variant="ghost">
                                 <Icon
                                     icon="radix-icons:moon"
                                     class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
