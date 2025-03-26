8c737dca5f5724a3ddbf868824dd91862dd27c0a
mehdighallam6 <mehdighallam6@gmail.com>
2025-03-26 23:08:50 +0000

    Fix

--- app/Http/Controllers/AdminUser/UserController.php
+++ app/Http/Controllers/AdminUser/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        return Inertia::render('Users/Edit', [
+        return Inertia::render('AdminUser/Users/Edit', [
             'user' => $user->load('roles:name'),
             'roles' => RolesEnum::cases(),
         ]);
