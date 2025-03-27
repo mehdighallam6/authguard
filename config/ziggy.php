6702990885ab37d5c28bbe0ad305aba933bf1db6
mehdighallam6 <mehdighallam6@gmail.com>
2025-03-27 14:21:59 +0000

    Create ziggy.php

---
+++ config/ziggy.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'only' =>  ['login', 'register', 'password.request', 'password.reset', 'password.email', 'verification.notice', 'verification.verify', 'verification.resend'],
+];
